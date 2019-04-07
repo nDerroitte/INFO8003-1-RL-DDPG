2da48cce86801079835a59737b7065b938118475
Natan <natan.derroitte@gmail.com>
2019-04-07 18:23:06 +0200

    Video making

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 import pygame
+import cv2
 from constants import CST
 
 class GUI:
-    def __init__(self, width, height, bar_width, bar_height, fruit_size):
+    def __init__(self, width, height, bar_width, bar_height, fruit_size, dt):
         self.width = width
         self.height = height
         self.bar_size = (bar_width, bar_height)
@@ -11,10 +12,12 @@ class GUI:
         self.window_size = (width, height)
         self.window = pygame.display.set_mode(self.window_size)
         self.run_time = pygame.time.Clock()
+        self.stream_img = []
+        self.dt = dt
 
     def updateGUI(self, environemnt):
         self.__draw(environemnt)
-        self.run_time.tick(environemnt.dt)
+        self.run_time.tick(self.dt)
 
     def __draw(self, environemnt):
         self.window.fill(CST.SCREEN_COLOR)
@@ -23,8 +26,26 @@ class GUI:
         pygame.draw.rect(self.window , CST.BAR_COLOR, (bar_position, self.bar_size))
         pygame.draw.rect(self.window , CST.FRUIT_COLOR, (fruit_position, self.fruit_size))
         pygame.display.flip()
+        # Video settings
+        img = pygame.surfarray.array3d(pygame.display.get_surface())
+        img = img.swapaxes(0, 1)
+        self.stream_img.append(img)
 
 
     def closeGUI(self):
         pygame.display.quit()
         pygame.quit()
+
+    def makeVideo(self, name):
+        print("Creating Video ..", end=' ')
+        # Get video stream dimensions from the image size
+        if len(self.stream_img) > 0:
+            height, width, layer = self.stream_img[0].shape
+            size = (width, height)
+            fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+            out = cv2.VideoWriter(name + '.avi', fourcc, self.dt, size)
+            for i in range(len(self.stream_img)):
+                out.write(cv2.cvtColor(self.stream_img[i], cv2.COLOR_RGB2BGR))
+            out.release()
+            print("done!")
+            cv2.destroyAllWindows()
--- run.py
+++ run.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
     game = ContinuousCatcher()
     d = False
     gui = GUI(game.width, game.height, game.bar_width, game.bar_height,
-              game.fruit_size)
+              game.fruit_size, game.dt)
     gui.updateGUI(game)
     while d == False:
         act = game.observe()
         a, r, d = game.step(act)
         gui.updateGUI(game)
+    gui.makeVideo("VideoName")
     gui.closeGUI()
